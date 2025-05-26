718661793a23719088e89368804fb8a5b6579c75
ahmedaidara05 <ahmedsaidaidara5@gmail.com>
2025-05-26 23:18:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
         1: {
             ar: "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ<br>ٱلْحَمْدُ لِلَّهِ رَبِّ ٱلْعَٰلَمِينَ",
             en: "In the name of Allah, the Most Gracious, the Most Merciful<br>Praise be to Allah, the Lord of all the worlds",
-            fr: "Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux<br>Louange à Allah, Seigneur des mondes"
+            fr: "Au nom d'Allah, , le Très Miséricordieux<br>Louange à Allah, Seigneur des mondes"
         },
         2: {
             ar: "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ<br>ذَٰلِكَ ٱلْكِتَٰبُ لَا رَيْبَ ۖ فِيهِ هُدًى لِّلْمُتَّقِينَ",
             en: "In the name of Allah, the Most Gracious, the Most Merciful<br>This is the Book about which there is no doubt, a guidance for those conscious of Allah",
-            fr: "Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux<br>Ceci est le Livre au sujet duquel il n'y a aucun doute, un guide pour les pieux"
+            fr: "Au nom d'Allah, le Tout Miséricordieux, leeux<br>Ceci est le Livre au sujet duquel il n'y a aucun doute, un guide pour les pieux"
         },
         3: {
             ar: "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ<br>الم ۝ ٱللَّهُ لَآ إِلَٰهَ إِلَّا هُوَ ٱلْحَىُّ ٱلْقَيُّومُ",
